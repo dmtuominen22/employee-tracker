4f8ef3d4b530553bcab795cc1361a068cca69cbe
Denise <dmtuominen@gmail.com>
2021-09-02 12:26:08 -0600

    added role table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,22 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
 
+CREATE TABLE employee (
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL.
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER,
+manager_id INTEGER
+);
+ 
+CREATE TABLE role (
+id INTEGER PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INTEGER
+);
+
+CREATE TABLE department (
+id INTEGER PRIMARY KEY,
+name VARCHAR(30)
+);
